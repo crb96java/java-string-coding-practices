a8679ef5630e37f71280a2ff78756e4d17ba9d57
crb96java <crb96java@gmail.com>
2024-09-26 00:00:39 +0530

    Added new class for remove duplicate words from String

---
+++ src/com/string/RemoveDuplicateWordsFromString.java
@@ -0,0 +1,25 @@
+package com.string;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicateWordsFromString {
+    public static void main(String[] args) {
+        //Created String Value
+        String str = "Hello World and Hero and Hello India";
+
+        //Created Set Object
+        Set<String> setValues = new HashSet<String>();
+
+        //Convert that String words into List of String
+        List<String> listOfStrings = Arrays.stream(str.split(" "))
+                .collect(Collectors.toList());
+
+        //Logic for to remove the duplicate words from the List of Strings
+        String response = listOfStrings.stream().filter(setValues::add).collect(Collectors.joining(" "));
+        System.out.println(response);
+    }
+}

bb7db7be78faa50593371e8cfc7ba47bbb2b6108
crb96java <crb96java@gmail.com>
2024-09-26 00:24:35 +0530

    Updated the code to print the list object into descending orders to remove the warning messages

--- src/com/collections/SortTheListObject.java
+++ src/com/collections/SortTheListObject.java
@@ -1,6 +1,7 @@
 package com.collections;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortTheListObject {
@@ -14,7 +15,7 @@ public class SortTheListObject {
 
         System.out.println("Print the list of data in descending order");
         //logic for print the list of Strings in descending order
-        list.stream().sorted((l1,l2)->l2.compareTo(l1)).forEach(System.out::println);
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
     }
 }
